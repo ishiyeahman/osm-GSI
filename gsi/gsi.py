f825eafb0ac8af3cc6fef86e99537c47b928afd1
ishiyeahman <ishiyeahman@gmail.com>
2023-01-04 22:45:52 +0900

    finished Multi Thread

--- gsi/gsi.py
+++ gsi/gsi.py
@@ -1,19 +1,29 @@
 import requests
 import asyncio
 import concurrent.futures
+from itertools import repeat
+
+config_get_max_depth_keys = ["Depth", "OfficeCode", "RiverCode", "SubRiverCode",  "CSVScale"]
 
 #  longitude , latitude 
 def get_max_depth(LON, LAT, key=None):
     url = f"https://suiboumap.gsi.go.jp/shinsuimap/Api/Public/GetMaxDepth?lon={LON}&lat={LAT}"
-    response = requests.get(url)
+    
+    
+    try:
+        response = requests.get(url, timeout=300)
+    except :
+        print(f"get_max_depth : {LON} {LAT} : exception!")
+        return None
+        
     jsonData = response.json()
     print(f"get_max_depth : {LON} {LAT} : {jsonData}")
-    if key:
+    if key in config_get_max_depth_keys:
         if jsonData:
             return jsonData[key]
         else :
-            return jsonData
-            # return  None
+            # return jsonData
+            return  None
     
     return jsonData
 
@@ -48,42 +58,17 @@ async def get_multi_max_depth(LONS, LATS, key=None):
 
 def get_max_depth_multi_thread(LONS, LATS, key=None):
     len_argument =  len(LONS)
-    MAX_WORKERS = 10000
+    MAX_WORKERS = 200
     
-    results = []
-    """
+    processed = []
+    
     with concurrent.futures.ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
-        # Start the load operations and mark each future with its URL
-        # future_to_url = {executor.submit(load_url, url, 60): url for url in URLS}
+        results = executor.map( get_max_depth, LONS, LATS, repeat(key))
         
-        # future_gmd =  dict(map(get_max_depth, LONS, LATS, key))
-        
-        future_gmd = {executor.submit( get_max_depth, lon, 32.82554, key) : lon for lon in LONS}
-        # future_gmd = {executor.submit( get_max_depth, LONS, LATS, [key]*len_argument) }
-        print(future_gmd)
-        
-        
-        for future in concurrent.futures.as_completed(future_gmd):
-            jsonData = future_gmd[future]
-            print(f"future - jsonData : {jsonData}")
-            results += [jsonData ]
+        for result in results:
+            processed.append(result)
             
-            try:
-                return results
-            except Exception as exc:
-                print('get_max_depth_multi_thread : error')
-            
-    """
+
     
-    with concurrent.futures.ThreadPoolExecutor(max_workers= MAX_WORKERS) as executor:
-        # futures = [executor.submit(target_func, x) for x in range(8)]
-        future_gmd = [executor.submit( get_max_depth, lon, 32.82554, key) for lon in LONS]
-        (done, notdone) = concurrent.futures.wait(future_gmd)
-        for future in future_gmd:
-            # print(future.result())
-            results += [ future.result()]
-    
-    return results
-            
-    
+    return processed
 
